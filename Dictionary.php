cdcc899dca53cbc8aa1cf2acb11ec446a3762482
lifenglin <364613883@qq.com>
2014-01-26 14:32:23 +0800

    增加异常处理

--- Dictionary.php
+++ Dictionary.php
@@ -12,11 +12,19 @@ class Dictionary
 
     public function __construct($arrDictionary = array())
     {
+        //检查输入
+        if (!is_array($arrDictionary)) {
+            throw new InvalidArgumentException('__construct() expects parameter 1 to be array');
+        }
         $this->_arrDictionary = $arrDictionary;
     }
 
     public function checkParams($arrParams = array())
     {
+        //检查输入
+        if (!is_array($arrParams)) {
+            throw new InvalidArgumentException('checkParams() expects parameter 1 to be array');
+        }
         //返回参数
         $arrReturn = array();
 
@@ -37,17 +45,17 @@ class Dictionary
             }
             //检查类型
             if (self::INT === $strType) {
-                $mixParam = $this->checkInt($mixParam);
+                $mixParam = $this->checkInt($strKey, $mixParam);
             } else if (self::UINT === $strType) {
-                $mixParam = $this->checkUint($mixParam);
+                $mixParam = $this->checkUint($strKey, $mixParam);
             } else if (self::ID === $strType) {
-                $mixParam = $this->checkId($mixParam);
+                $mixParam = $this->checkId($strKey, $mixParam);
             } else if (self::STRING === $strType) {
                 $mixParam = strval($mixParam);
             } else if (self::DATE === $strType) {
-                $mixParam = $this->checkDate($mixParam);
+                $mixParam = $this->checkDate($strKey, $mixParam);
             } else {
-                //throw
+                throw new UnexpectedValueException('unexpected type');
             }
             $arrReturn[$strKey] = $mixParam;
         }
@@ -62,10 +70,10 @@ class Dictionary
      * @access public
      * @return void
      */
-    static public function checkInt($mixParam)
+    static public function checkInt($strKey, $mixParam)
     {
         if (!ctype_digit(strval($mixParam))) {
-            //throw
+            throw new DomainException("$strKey is not a int");
         }
         return intval($mixParam);
     }
@@ -78,11 +86,15 @@ class Dictionary
      * @access public
      * @return void
      */
-    static public function checkUint($mixParam)
+    static public function checkUint($strKey, $mixParam)
     {
-        $intParam = self::checkInt($mixParam);
+        try {
+            $intParam = self::checkInt($strKey, $mixParam);
+        } catch (DomainException $e) {
+            throw new DomainException("$strKey is not a uint");
+        }
         if ($intParam < 0) {
-            //throw
+            throw new DomainException("$strKey is not a uint and less than 0");
         }
         return $intParam;
     }
@@ -95,12 +107,12 @@ class Dictionary
      * @access public
      * @return void
      */
-    static public function checkId($mixParam)
+    static public function checkId($strKey, $mixParam)
     {
         try {
             $mixParam = new MongoId($mixParam);
-        } catch (exception $e) {
-            //throw
+        } catch (MongoException $e) {
+            throw new DomainException("$strKey is not a id");
         }
         return $mixParam;
     }
@@ -111,11 +123,11 @@ class Dictionary
      * @access public
      * @return void
      */
-    static public function checkDate($mixParam)
+    static public function checkDate($strKey, $mixParam)
     {
         $mixParam = strtotime($mixParam);
         if (false === $mixParam) {
-            //throw
+            throw new DomainException("$strKey is not a date");
         }
         return $mixParam;
     }
